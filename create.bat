362d1cefdeb6732fb70d392fc8dfc27d111d7949
frankofilok <frankofilok@Frankofil.myslek.net>
2020-04-25 22:59:31 +0200

    Linking command to python script

--- create.bat
+++ create.bat
@@ -2,13 +2,14 @@
 
 set project_name=%1
 set flag=%2
+cd /d %~dp0
 
 if "%1"=="" (
-    echo "error"
+    echo "Error 01: no repository name"
 )  else (
-    if "%2"=="" (
-        echo "no parameter"
+    if "%2"=="-public" (
+        python create.py %project_name% %flag%
     ) else (
-        echo "parameter %2%"
+            python create.py %project_name% 
+        )
     )
-)
--- create.py
+++ create.py
@@ -1,7 +1,15 @@
+import sys
+
+
 def main():
-    pass
+    public = False
+    projectName = sys.argv[1]
+    if len(sys.argv) > 2 and sys.argv[2] == "-public":
+        public = True
+
+    print(projectName)
+    print(public)
 
 
 if __name__ == '__main__':
     main()
-    
